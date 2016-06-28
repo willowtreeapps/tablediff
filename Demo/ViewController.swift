fa8c9cde4d19d8abca8c62b04fad6e49691b6e57
Kent White <kent.white@willowtreeapps.com>
2016-06-28 10:09:30 -0400

    Add flag to deep diff method for pre or post update

---
+++ Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Kent White on 7/5/16.
+//  Copyright © 2016 WillowTree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+    }
+    
+}
+
