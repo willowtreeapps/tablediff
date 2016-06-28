ba39c5b09f2bf176c1e189e6ab8ed38e0f26f60f
Ian Terrell <ian.terrell@willowtreeapps.com>
2016-06-28 11:49:12 -0400

    Add simple move tests

--- DeepDiffTests/Helpers.swift
+++ DeepDiffTests/Helpers.swift
@@ -15,6 +15,20 @@ extension Int: SequenceDiffable {
     }
 }
 
+struct Widget: SequenceDiffable {
+    let id: String
+    var name: String
+    var price: Int
+
+    func identifiedSame(other: Widget) -> Bool {
+        return id == other.id
+    }
+}
+
+func ==(lhs: Widget, rhs: Widget) -> Bool {
+    return lhs.id == rhs.id && lhs.name == rhs.name && lhs.price == rhs.price
+}
+
 func randomArray(minSize: Int = 0, maxSize: Int = 100, minValue: Int = 0, maxValue: Int = 1000) -> [Int] {
     let count = UInt32(minSize) + arc4random_uniform(UInt32(maxSize-minSize))
     return (0..<count).map { _ in minValue + Int(arc4random_uniform(UInt32(maxValue - minValue))) }
---
+++ DeepDiffTests/MoveTests.swift
@@ -0,0 +1,50 @@
+//
+//  MoveTests.swift
+//  DeepDiff
+//
+//  Created by Ian Terrell on 6/28/16.
+//  Copyright © 2016 WillowTree. All rights reserved.
+//
+
+import XCTest
+import DeepDiff
+
+class MoveTests: XCTestCase {
+
+    func testMoveHead() {
+        let x: [Int] = [0, 1, 2, 3]
+        let y: [Int] = [1, 2, 3, 0]
+        let (diff, updates) = x.deepDiff(y)
+        XCTAssertEqual([DiffStep.move(fromIndex: 0, toIndex: 3)], diff)
+        XCTAssertEqual([], updates)
+    }
+
+    func testMoveTail() {
+        let x: [Int] = [1, 2, 3, 0]
+        let y: [Int] = [0, 1, 2, 3]
+        let (diff, updates) = x.deepDiff(y)
+        XCTAssertEqual([DiffStep.move(fromIndex: 3, toIndex: 0)], diff)
+        XCTAssertEqual([], updates)
+    }
+
+    func testMoveMiddle() {
+        let x: [Int] = [1, 2, 0, 3]
+        let y: [Int] = [1, 0, 2, 3]
+        let (diff, updates) = x.deepDiff(y)
+        XCTAssertEqual([DiffStep.move(fromIndex: 2, toIndex: 1)], diff)
+        XCTAssertEqual([], updates)
+    }
+
+    func testMoveMultiple() {
+        let x: [Int] = [1, 2, 3]
+        let y: [Int] = [3, 2, 1]
+        let (diff, updates) = x.deepDiff(y)
+        let expectedDiff: [DiffStep<Int,Int>] = [
+            DiffStep.move(fromIndex: 0, toIndex: 2),
+            DiffStep.move(fromIndex: 2, toIndex: 0),
+            ]
+        XCTAssertEqual(expectedDiff, diff)
+        XCTAssertEqual([], updates)
+    }
+
+}
